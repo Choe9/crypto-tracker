4afadd6294d69ab78cff84437b0c343b8d6b595a
DESKTOP-NSNJVU7\runcj <run1529@gmail.com>
2023-05-10 14:44:55 +0900

    react create crypto-tracker

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+//import App from "./Root";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/routes/Chart.tsx
@@ -0,0 +1,104 @@
+import { useQuery } from "react-query";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ReactApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId?: string;
+}
+
+function Chart() {
+  const { coinId } = useOutletContext() as ChartProps;
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    // ["ohlcv", "btc-bitcoin"],
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    // () => fetchCoinHistory("btc-bitcoin"),
+    {
+      refetchInterval: 10000,
+    }
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ReactApexChart
+          type="candlestick"
+          series={[
+            {
+              data: data?.map((price) => {
+                return {
+                  x: new Date(price.time_close * 1000),
+                  y: [
+                    Number(price.open),
+                    Number(price.high),
+                    Number(price.low),
+                    Number(price.close),
+                  ],
+                };
+              }),
+            } as { data: { x: Date; y: number[] }[] },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "rgba(0, 0, 0, 0.5)",
+            },
+            // grid: { show: false },
+            // stroke: {
+            //   curve: "smooth",
+            //   width: 4,
+            // },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+            },
+            // fill: {
+            //   type: "gradient",
+            //   gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            // },
+            // colors: ["#0fbcf9"],
+            // tooltip: {
+            //   y: {
+            //     formatter: (value) => `$ ${value.toFixed(3)}`,
+            //   },
+            // },
+            plotOptions: {
+              candlestick: {
+                colors: {
+                  upward: "#DF7D46",
+                  downward: "#3C90EB",
+                },
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chart;
